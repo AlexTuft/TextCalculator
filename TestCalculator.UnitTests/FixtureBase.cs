65d4b73ce433db3f4d2f0e89f81700728fc43080
Alex Tuft <tufty94@gmail.com>
2020-11-05 20:10:34 +0000

    Add number literal

---
+++ TestCalculator.UnitTests/FixtureBase.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+
+namespace TextCalculator.UnitTests
+{
+    public class FixtureBase
+    {
+        protected readonly Fixture fixture = new Fixture();
+
+        protected FixtureBase()
+        {
+            var numberGenerator = new RandomNumericSequenceGenerator(-10000, 10000);
+            fixture.Register(numberGenerator.Create<int>);
+        }
+
+        protected TObj Create<TObj>() => fixture.Create<TObj>();
+    }
+}
