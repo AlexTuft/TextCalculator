036b07cfd3fee66a4e0f3dacd97025caa9b6b80d
Alex Tuft <tufty94@gmail.com>
2020-11-05 20:11:11 +0000

    Add multiplication operator

---
+++ TestCalculator.UnitTests/Expressions/Multiplication.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using TextCalculator.Expressions;
+using Xunit;
+
+namespace TextCalculator.UnitTests.Expressions
+{
+    public class Multiplication : FixtureBase
+    {
+        [Fact]
+        void ShouldReturnProductOfTwoExpressions()
+        {
+            var expression1 = Create<NumberLiteral>();
+            var expression2 = Create<NumberLiteral>();
+            var expectedResult = expression1.Result * expression2.Result;
+
+            var addition = new MultiplicationOperator(expression1, expression2);
+
+            addition.Result.Should().Be(expectedResult);
+        }
+    }
+}
