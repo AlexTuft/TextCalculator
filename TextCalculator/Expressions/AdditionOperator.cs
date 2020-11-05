24c83ef31c5932662ec5cca8b1cd0ca8387edd91
Alex Tuft <tufty94@gmail.com>
2020-11-05 20:10:49 +0000

    Add addition operator

---
+++ TextCalculator/Expressions/AdditionOperator.cs
@@ -0,0 +1,16 @@
+﻿namespace TextCalculator.Expressions
+{
+    public class AdditionOperator : IExpression
+    {
+        private IExpression _lhs;
+        private IExpression _rhs;
+
+        public AdditionOperator(IExpression lhs, IExpression rhs)
+        {
+            _lhs = lhs;
+            _rhs = rhs;
+        }
+
+        public double Result => _lhs.Result + _rhs.Result;
+    }
+}
