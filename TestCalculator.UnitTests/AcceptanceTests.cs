2d476f6a724e4ca93f065f6aa413df48b9091b62
Alex Tuft <tufty94@gmail.com>
2020-11-08 01:31:39 +0000

    Update acceptance tests

---
+++ TestCalculator.UnitTests/AcceptanceTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using TextCalculator.Parsing;
+using Xunit;
+
+namespace TextCalculator.UnitTests.Expressions
+{
+    public class AcceptanceTests : FixtureBase
+    {
+        [Theory]
+        [InlineData("2 + 4", 6)]
+        [InlineData("5 - 41", -36)]
+        [InlineData("32 * -45", -1440)]
+        [InlineData(" -12 + 45 - 3", 30)]
+        [InlineData("-3 + 100 / 4", 22)]
+        void RunCase(string input, double expectedOutput)
+        {
+            var parser = new Parser();
+            var expression = parser.Parse(input);
+
+            if (expression is null)
+            {
+                true.Should().BeFalse(); // FAIL
+            }
+            else
+            {
+                expression.Result.Should().Be(expectedOutput);
+            }
+        }
+    }
+}
