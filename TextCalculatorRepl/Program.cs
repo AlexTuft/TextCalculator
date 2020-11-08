7d008a5168ac22877466d818849d91a369872c20
Alex Tuft <tufty94@gmail.com>
2020-11-08 00:32:06 +0000

    Add REPL project

---
+++ TextCalculatorRepl/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TextCalculator.Parsing;
+
+namespace TextCalculatorRepl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+
+            while (true)
+            {
+                var input = GetInput();
+
+                try
+                {
+                    EvaluateAndPrintResult(parser, input);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static string GetInput()
+        {
+            Console.Write('>');
+            var input = Console.ReadLine();
+            return input;
+        }
+
+        private static void EvaluateAndPrintResult(Parser parser, string input)
+        {
+            var expression = parser.Parse(input);
+
+            if (expression != null)
+            {
+                Console.WriteLine(expression.Result);
+            }
+        }
+    }
+}
