1f708f5365817540f9363e75cc6cf58c305b9d66
Alex Tuft <tufty94@gmail.com>
2020-11-08 01:17:46 +0000

    Refactor number literal parser

--- TextCalculator/Parsing/NumberLiteralParser.cs
+++ TextCalculator/Parsing/NumberLiteralParser.cs
@@ -6,22 +6,42 @@ namespace TextCalculator.Parsing
     {
         public IExpression? Parse(InputReader input)
         {
-            string token = "";
+            var token = ReadIntegerPart(input) + ReadFractionalPart(input);
 
+            GuardIsNotOnlySignCharacter(input, token);
+            return ParseToken(token);
+        }
+
+        private static string ReadIntegerPart(InputReader input)
+        {
+            var token = "";
             token += input.TakeIf('-', '+');
             token += input.TakeWhile(char.IsDigit);
+            return token;
+        }
 
+        private static string ReadFractionalPart(InputReader input)
+        {
+            var token = "";
             if (input.NextIs('.'))
             {
                 token += input.TakeIf('.');
                 token += input.TakeWhile(char.IsDigit);
             }
 
+            return token;
+        }
+
+        private static void GuardIsNotOnlySignCharacter(InputReader input, string token)
+        {
             if (token == "+" || token == "-")
             {
                 throw new BadInputFormat(input.Text, input.Index);
             }
+        }
 
+        private static IExpression? ParseToken(string token)
+        {
             if (!string.IsNullOrEmpty(token))
             {
                 return new NumberLiteral(double.Parse(token));
